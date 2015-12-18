b0548b0fa80af527ce97cecde0a7278468d2d43b
erladion <johan.jansson94@gmail.com>
2015-12-18 11:17:50 +0100

    Update PDController.c
    Added comments

--- Velociraptor_3.0/Styrmodul/GccApplication1/PDController.c
+++ Velociraptor_3.0/Styrmodul/GccApplication1/PDController.c
@@ -10,9 +10,11 @@
 #include "SensorAlgorithm.h"
 
 int reglering(uint16_t irfl, uint16_t irfr, uint16_t irrl, uint16_t irrr){
-	
+	// How much the robot is standing relative to the middle
 	int position = corridor_side_position(irfl,irfr,irrl,irrr);
+	// How much the robot is skewed from the middle
 	int skewness = corridor_robot_skewness(irfl,irfr,irrl,irrr);
 	
+	// Determines how much the robot is off the centerline and how skewed it is
 	return SCALEFACTOR*(position + KD*skewness);
 }
