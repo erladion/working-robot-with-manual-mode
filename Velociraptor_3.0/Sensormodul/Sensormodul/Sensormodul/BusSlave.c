79d7e0c1f5ba226e79ad4251ca7799841f9c55ec
Dandidodo <dandidodo@hotmail.com>
2015-12-18 11:04:10 +0100

    Removed some test code

--- Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/BusSlave.c
+++ Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/BusSlave.c
@@ -2,7 +2,8 @@
  * BusSlave.c
  *
  * Created: 11/30/2015 4:06:27 PM
- *  Author: filma035
+ * Author: filma035, marlu642
+ * Version: 1.0
  */ 
 
 #include <avr/interrupt.h>
@@ -13,11 +14,10 @@
 
 ISR(TWI_vect){
 	// SEND DATA - sensormodul
-	pulse(PORTC, 7);
+	
 	// The Slave has been addressed by the Master
 	if ((TWSR & 0xF8) == 0xA8){ // use TW_ST_DATA_ACK instead of 0xA8 maybe?
 		sending = 1;
-		pulse(PORTD,0);
 		TWIcounter = 0;
 		TWDR = TWIdata[TWIcounter];
 		TWIcounter++;
@@ -25,8 +25,6 @@ ISR(TWI_vect){
 	}
 	// Send data now
 	else if ((TWSR & 0xF8) == 0xB8){
-		pulse(PORTC, 7);
-		pulse(PORTC, 7);
 		TWDR = TWIdata[TWIcounter];
 		TWIcounter++;
 		TWCR = (1<< TWINT) | (1 << TWEA) | (1 << TWIE) | (1 << TWEN);
@@ -63,7 +61,6 @@ ISR(TWI_vect){
 void initBuss(char address){
 	DDRC |= (1 << 7);
 	DDRD |= (1 << 0);
-	//pulse(PORTC, 7);
 	sending = 0;
 	
 	TWAR = address;
@@ -71,5 +68,4 @@ void initBuss(char address){
 	TWCR |= (1 << TWINT); // clear TWINT
 	cbi(TWCR, TWSTA);
 	cbi(TWCR, TWSTO);
-	//pulse(PORTC, 7);
 }
