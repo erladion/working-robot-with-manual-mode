5428e4b5d0211864f197ea2099d9e505398d7821
Fippe94 <Fippe94@spray.se>
2015-12-18 12:48:43 +0100

    Update GrippingClaw.h

--- Velociraptor_3.0/Styrmodul/GccApplication1/GrippingClaw.h
+++ Velociraptor_3.0/Styrmodul/GccApplication1/GrippingClaw.h
@@ -9,7 +9,11 @@
 #ifndef GRIPPINGCLAW_H_
 #define GRIPPINGCLAW_H_
 
+// Make the gripping claw move towards claw_position.
+// Good values are somewhere between 250-400. 
+// At 250 the claw is closed shut and at 400 the claw is wide open aka claw position.
 void grab_object(int claw_position);
+// Initializes the gripping claw
 void init_gripping_claw_timer();
 
 #endif /* GRIPPINGCLAW_H_ */
