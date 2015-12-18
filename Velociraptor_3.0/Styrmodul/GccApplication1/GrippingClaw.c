285519191e77ea40c3369af4775cc1607f093e16
Fippe94 <Fippe94@spray.se>
2015-12-18 12:49:59 +0100

    Update GrippingClaw.c

--- Velociraptor_3.0/Styrmodul/GccApplication1/GrippingClaw.c
+++ Velociraptor_3.0/Styrmodul/GccApplication1/GrippingClaw.c
@@ -14,23 +14,10 @@
 // Servos are another example. They tend to expect a pulse every 20ms - depending on the width of the pulse they move to a given location.
 
 
-// Make the gripping claw move towards claw_position.
-// Good values are somewhere between 250-400. 
-// At 250 the claw is closed shut and at 400 the claw is wide open aka claw position.
 void grab_object(int claw_position){
 	OCR1A = claw_position;
-	
-	// EMERGENCY SOLUTION IF ALL ELSE FAILS
-	//for (int i=0; i < 6; i++)
-	//{
-		//sbi(PORTD, 5);
-		//_delay_ms(1);
-		//cbi(PORTD, 5);
-		//_delay_ms(20);
-	//}
 }
 
-// Initializes the gripping claw
 void init_gripping_claw_timer()
 {	
 	DDRD |= (1<<PD5); // Set PWM pin as output
@@ -46,11 +33,6 @@ void init_gripping_claw_timer()
 	TCCR1A |= (1<<COM1A1)|(1<<WGM11);	// Set timer1 to non inverted PWM
 	TCCR1B |= (1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); // Prescale with 64. Set mode to 14(FAST PWM)
 	
+	// Set the gripping claw to "open" mode as starting value
 	grab_object(350);
-	//DDRD |= (1<<PD5);
-	//TCNT1 = 0;
-	//ICR1 = 4607;
-	//
-	//TCCR1A = _BV(WGM11) | _BV(COM1A1);
-	//TCCR1B = (1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10);
 }
