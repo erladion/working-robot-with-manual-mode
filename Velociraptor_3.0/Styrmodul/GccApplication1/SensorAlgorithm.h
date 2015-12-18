746a295e012eb3ccf2d35b5e5ac1f49da2d617a7
Fippe94 <Fippe94@spray.se>
2015-12-18 12:37:55 +0100

    Update SensorAlgorithm.h

--- Velociraptor_3.0/Styrmodul/GccApplication1/SensorAlgorithm.h
+++ Velociraptor_3.0/Styrmodul/GccApplication1/SensorAlgorithm.h
@@ -8,7 +8,9 @@
 #ifndef SENSORALGORITHM_H_
 #define SENSORALGORITHM_H_
 
+// Gives a value of how skewed the robot is in relation to the walls
 int corridor_robot_skewness(int A, int B, int C, int D);
+// Gives a value of how far away from the middle the robot is
 int corridor_side_position(int A, int B, int C, int D);
 
 #endif /* SENSORALGORITHM_H_ */
