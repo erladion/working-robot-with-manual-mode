45e25ff8cc7b410a1b1e4e97034dc3b26b201d56
erladion <johan.jansson94@gmail.com>
2015-12-18 11:01:22 +0100

    Update IRSensor.h
    Added comments

--- Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/IRSensor.h
+++ Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/IRSensor.h
@@ -9,13 +9,20 @@
 #ifndef IRSENSOR_H_
 #define IRSENSOR_H_
 
+// IR-front left value
 volatile uint8_t IRFL;
+// IR-front right value
 volatile uint8_t IRFR;
+// IR-back left value
 volatile uint8_t IRBL;
+// IR-back right value
 volatile uint8_t IRBR;
 
+// Initiate the A/D converter for the ir
 void irInit();
+// Loop through the pins/channels to which the ir sensors are connected to
 void readAllIr();
+// Read the A/D value for the specified channel 
 int irRead(uint8_t channel);
 
 #endif /* IRSENSOR_H_ */
