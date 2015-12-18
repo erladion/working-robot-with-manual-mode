cf34372dd844ebd35dff552be80383eb45d4f9d1
Dandidodo <dandidodo@hotmail.com>
2015-12-18 12:10:03 +0100

    Added comments for the functions

--- Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/UltrasoundSensor.h
+++ Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/UltrasoundSensor.h
@@ -12,9 +12,13 @@
 volatile double distanceFront;
 volatile double distanceBack;
 
+// Initialize timer 1
 void timer1_init();
+//  Enable interrupts for PC6 for the front sensor and PA7 for the rear sensor
 void ext_interrupt_init();
+// Calculate the distance in centimeters based on the timer value
 double calculate_distance(volatile int end_value);
+// Initialize ultrasound
 void ultrasoundInit();
 
 
