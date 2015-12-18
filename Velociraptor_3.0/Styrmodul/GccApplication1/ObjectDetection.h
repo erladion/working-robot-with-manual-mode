9dfe85148489d55f24582c8cece4b4d28a28780e
Fippe94 <Fippe94@spray.se>
2015-12-18 12:43:02 +0100

    Update ObjectDetection.h

--- Velociraptor_3.0/Styrmodul/GccApplication1/ObjectDetection.h
+++ Velociraptor_3.0/Styrmodul/GccApplication1/ObjectDetection.h
@@ -2,8 +2,11 @@
 #ifndef OBJECTDETECTION_H_
 #define OBJECTDETECTION_H_
 
+// checks how many of the 11 sensors that are active, between a startindex and a stopindex (the stopindex are not included in the check)
 int activatedSensors(int reflexData, int startIndex, int stopIndex);
+// Checks if we can see tape
 bool objectFinder(int reflexData);
+// Follows the middle tape as well as possible, and goes to grab mode as soon as we have found the second tape
 void objectCentering(int reflexData);
 
 
