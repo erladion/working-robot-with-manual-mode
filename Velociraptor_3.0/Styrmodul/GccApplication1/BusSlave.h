c773a6cc53fd688198d1dac8891ac119d311f152
Dandidodo <dandidodo@hotmail.com>
2015-12-18 13:45:50 +0100

    Added comments for the functions

--- Velociraptor_3.0/Styrmodul/GccApplication1/BusSlave.h
+++ Velociraptor_3.0/Styrmodul/GccApplication1/BusSlave.h
@@ -9,13 +9,13 @@
 #ifndef BUSSLAVE_H_
 #define BUSSLAVE_H_
 
-#define SensorSize 13
-
+// Variable for storing the sensor data that's going to be received from the communications module
 volatile char* TWIdata;
+// Variable for indexing through TWIdata
 volatile int TWIcounter;
-
+// Initialize the bus
 void initBuss(char address);
-
+// Variable for checking if we're receiving sensor values or bluetooth values
 volatile int sensor;
 
 #endif /* BUSSLAVE_H_ */
