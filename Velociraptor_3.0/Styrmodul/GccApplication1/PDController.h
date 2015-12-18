2295a34369ac916ea68dd521b45e889d67c0c9be
erladion <johan.jansson94@gmail.com>
2015-12-18 11:15:19 +0100

    Update PDController.h

--- Velociraptor_3.0/Styrmodul/GccApplication1/PDController.h
+++ Velociraptor_3.0/Styrmodul/GccApplication1/PDController.h
@@ -8,9 +8,12 @@
 #ifndef PDCONTROLLER_H_
 #define PDCONTROLLER_H_
 
-#define KD 2.5// Was 0.8 2015-12-15
+// How much the robot will react on 
+#define KD 2.5
+// Scale factor which scales the whole return value
 #define SCALEFACTOR 4
 
+// Returns a value for deciding how we are standing in the corridor
 int reglering(uint16_t irfl, uint16_t irfr, uint16_t irrl, uint16_t irrr);
 
 #endif /* PDCONTROLLER_H_ */
