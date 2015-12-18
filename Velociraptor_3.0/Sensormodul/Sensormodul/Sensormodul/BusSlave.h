a30249c78699f3682c102fb2070c5b2438124462
Dandidodo <dandidodo@hotmail.com>
2015-12-18 11:05:54 +0100

    Added descriptions for the variables and functions

--- Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/BusSlave.h
+++ Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/BusSlave.h
@@ -2,22 +2,18 @@
  * BusSlave.h
  *
  * Created: 11/30/2015 4:20:57 PM
- *  Author: filma035
+ * Author: filma035, marlu642
+ * Version: 1.0
  */ 
 
 
 #ifndef BUSSLAVE_H_
 #define BUSSLAVE_H_
 
-#define SensorSize 13
+volatile int sending; // Variable for checking if data is currently being sent or not
+volatile char* TWIdata; // Variable for storing the sensor data that's going to be sent to the communications module
+volatile int TWIcounter; // Variable for indexing through TWIdata
 
-int test;
-
-volatile int sending;
-volatile char* TWIdata;
-volatile int TWIcounter;
-
-void initBuss(char address);
-
+void initBuss(char address); // Initialize the bus
 
 #endif /* BUSSLAVE_H_ */
