9a09832329d283bca9d65802c58cda3760554496
erladion <johan.jansson94@gmail.com>
2015-12-18 11:04:37 +0100

    Update DebugHelp.h
    Comments

--- Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/DebugHelp.h
+++ Velociraptor_3.0/Sensormodul/Sensormodul/Sensormodul/DebugHelp.h
@@ -16,7 +16,9 @@
 // Sends a pulse on the specified pin in the specified port, used for debugging
 #define pulse(port, bit) cbi(port,bit); sbi(port, bit); cbi(port,bit);
 
+// Converts an integer into a bitarray of the specified size and saves it in the specified array
 void intToBitArray(int array[], int arraySize, int num);
+// Converts a bitarray into an integer
 int bitArrayToInt(int array[], int arraySize);
 
 #endif /* DEBUGHELP_H_ */
